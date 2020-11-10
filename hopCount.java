4093695d6d8cbddf64bfa8b3e73ab88bac5eddf2
nerdhiqedme <74059692+nerdhiqedme@users.noreply.github.com>
2020-11-10 08:51:53 +0300

    assingment3
    A mouse is looking over a river that's 21 feet wide. There are 20 stones placed in the river 1 foot apart,
    such that there is 1 foot from the beginning of the river to the first stone, and 1 foot from the first stone
    to the next stone, and so on; and 1 foot from the last stone to the end of the river. The mouse can jump
    over 1 foot or 2 feet such that the first step the mouse can make is either from the beginning of the
    river to the first stone, or from the beginning of the river to the second stone. If the mouse can't jump
    backwards, write a Java function that returns the number of different ways the mouse can jump from
    the beginning of the river to the end of the river.

---
+++ hopCount.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zerakiassessment;
+
+import java.util.List;
+import java.util.Scanner;
+import static zerakiassessment.reverseCharacters.splitToFour;
+
+/**
+ *
+ * @author Eclectics
+ */
+public class hopCount {
+     public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Input number of stones:");
+
+// String input
+        int Stones = myObj.nextInt();
+    
+       
+        int result = numberOfCOunt(Stones);
+        System.out.println(result);
+    }
+    private static int numberOfCOunt(int stones){
+        int result = 0;
+        
+        
+        return result;
+        
+    }
+}
